b1e404293741e4e28cf73e6b92c1489cdab29783
John Trinidad <john.trinidad1@upr.edu>
2024-04-22 13:10:41 -0400

    app module for heroku

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,38 +1,46 @@
-/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Todo } from './todo/entities/todo.entity';
 import { InterestModule } from './interest/interest.module';
 import { PostModule } from './post/post.module';
-import { Post } from './post/entities/post.entity';
 import { CommunityModule } from './community/community.module';
-import { Community } from './community/entities/community.entity';
 import { UsersModule } from './users/users.module';
-import { User } from './users/entities/user.entity';
 import { CommunityMembersModule } from './community_members/community_members.module';
+import { Todo } from './todo/entities/todo.entity';
+import { Post } from './post/entities/post.entity';
+import { Community } from './community/entities/community.entity';
+import { User } from './users/entities/user.entity';
 import { CommunityMember } from './community_members/entities/community_member.entity';
-
+import { Interest } from './interest/entities/interest.entity';
 
 @Module({
-  imports: [TypeOrmModule.forRootAsync({
-    imports: [ConfigModule],
-    useFactory: (configService: ConfigService) => ({
-      type: 'postgres',
-      host: configService.get('DATABASE_HOST'),
-      port: +configService.get('DATABASE_PORT'),
-      username: configService.get('DATABASE_USER'),
-      password: configService.get('DATABASE_PASSWORD'),
-      database: configService.get('DATABASE_NAME'),
-      entities: [Todo, Post,Community,User,CommunityMember ], 
-      synchronize: true,
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get('DATABASE_URL'), 
+        entities: [Todo, Post, Community, User, CommunityMember,Interest],
+        synchronize: process.env.NODE_ENV !== 'production',
+        ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+      }),
+      inject: [ConfigService],
     }),
-    inject: [ConfigService]
-  }), ConfigModule,ConfigModule.forRoot({ envFilePath: ['.env']}), TodoModule, InterestModule, PostModule, CommunityModule, UsersModule, CommunityMembersModule],
+    ConfigModule.forRoot({
+      envFilePath: process.env.NODE_ENV === 'development' ? ['.env'] : undefined,
+      ignoreEnvFile: process.env.NODE_ENV === 'production',
+    }),
+    TodoModule,
+    InterestModule,
+    PostModule,
+    CommunityModule,
+    UsersModule,
+    CommunityMembersModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService], 
 })
 export class AppModule {}
